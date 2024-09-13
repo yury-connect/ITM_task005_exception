58408ac8783854acf2e604621a53f94eeb1fd791
Yury <yury.connect@gmail.com>
2024-09-13 23:24:38 +0300

    Solutions: task0902 - подчистил код

--- 08 Знакомство с исключениями try, catch, throws, multy-catch/task0902/src/task0902/Solution.java
+++ 08 Знакомство с исключениями try, catch, throws, multy-catch/task0902/src/task0902/Solution.java
@@ -1,16 +1,16 @@
 package task0902;
 
 /* 
-И снова StackTrace
-Написать пять методов, которые вызывают друг друга.
-Каждый метод должен возвращать имя метода, вызвавшего его, полученное с помощью StackTrace.
+    И снова StackTrace
+    Написать пять методов, которые вызывают друг друга.
+    Каждый метод должен возвращать имя метода, вызвавшего его, полученное с помощью StackTrace.
 
-
-Requirements:
-1. В классе должно быть 5 методов (метод main не учитывать).
-2. Каждый метод должен вызывать следующий: main вызывать method1, method1 вызывать method2 и т.д.
-3. Каждый метод должен возвращать имя метода, вызвавшего его.
-4. Для получения имени вызвавшего метода, используй метод getMethodName.*/
+    Requirements:
+    1. В классе должно быть 5 методов (метод main не учитывать).
+    2. Каждый метод должен вызывать следующий: main вызывать method1, method1 вызывать method2 и т.д.
+    3. Каждый метод должен возвращать имя метода, вызвавшего его.
+    4. Для получения имени вызвавшего метода, используй метод getMethodName.
+*/
 
 
 public class Solution {
@@ -21,6 +21,7 @@ public class Solution {
     public static String method1() {
         return whoCalledMe() + " -> " + method2();
     }
+
     public static String method2() {
         return whoCalledMe() + " -> " + method3();
     }
@@ -37,9 +38,12 @@ public class Solution {
         return whoCalledMe();
     }
 
+
     private static String whoCalledMe() {
         String callingMethodName = getCallingMethodName(3);
-        System.out.printf("На связи метод: '%s'. Меня вызвал метод: '%s'\n", getCallingMethodName(2), callingMethodName);
+        System.out.printf("На связи метод: '%s'. Меня вызвал метод: '%s'\n"
+                , getCallingMethodName(2)
+                , callingMethodName)
         return callingMethodName;
     }
 
